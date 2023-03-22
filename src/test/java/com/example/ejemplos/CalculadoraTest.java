b4fe177244efe10e29d27a71376241f10b7ddb75
Daniel Jaimes Carrillo <daniel.jaimes-carrillo@capgemini.com>
2023-03-22 19:58:14 +0100

    add another entry paramaters in different posibilities of them

--- src/test/java/com/example/ejemplos/CalculadoraTest.java
+++ src/test/java/com/example/ejemplos/CalculadoraTest.java
@@ -18,4 +18,32 @@ public class CalculadoraTest {
         var rslt = calc.suma(2, 2);
         assertEquals(4, rslt);
     }
+
+    @Test
+    void testSumaPositivoNegativo() {
+        var calc = new Calculadora();
+        var rslt = calc.suma(3, -1);
+        assertEquals(2, rslt);
+    }
+
+    @Test
+    void testSumaNegativoPositivo() {
+        var calc = new Calculadora();
+        var rslt = calc.suma(-3, 3);
+        assertEquals(-0, rslt);
+    }
+
+    @Test
+    void testSumaNegativoNegativo() {
+        var calc = new Calculadora();
+        var rslt = calc.suma(-4, -3);
+        assertEquals(-7, rslt);
+    }
+
+    @Test
+    void testSumaDecimal() {
+        var calc = new Calculadora();
+        var rslt = calc.suma(0.2, 0.3);
+        assertEquals(0.5, rslt);
+    }
 }
