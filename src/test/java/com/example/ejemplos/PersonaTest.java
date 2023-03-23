96d85ac833c4bac4a6646b49e7f04fe8739edfee
Daniel Jaimes Carrillo <daniel.jaimes-carrillo@capgemini.com>
2023-03-23 15:00:43 +0100

    Refactor: - OK and KO

--- src/test/java/com/example/ejemplos/PersonaTest.java
+++ src/test/java/com/example/ejemplos/PersonaTest.java
@@ -1,9 +1,6 @@
 package com.example.ejemplos;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.RepetitionInfo;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,32 +10,39 @@ class PersonaTest {
     void setUp() {
     }
 
-    @Test
-    void testCreate() {
-        var p = Persona.builder().id(1).nombre("Daniel").apellidos("Jaimes").build();
-        assertNotNull(p);
-        assertTrue(p instanceof Persona, "No es instancia de persona");
+    @Nested
+    @DisplayName("Method Constructor Test")
+    public class Constructor {
 
-        assertAll("Validar propiedades",
-                () -> assertEquals(1, p.getId()),
-                () -> assertEquals("Daniel", p.getNombre()),
-                () -> assertEquals("Jaimes", p.getApellidos())
-        );
-    }
+        @Nested
+        public class OK {
+            @Test
+            void testCreate() {
+                var p = Persona.builder().id(1).nombre("Daniel").apellidos("Jaimes").build();
+                assertNotNull(p);
+                assertTrue(p instanceof Persona, "No es instancia de persona");
 
-    @RepeatedTest(value = 5, name = "{displayName} {currentRepetition} / {totalRepetitions}")
-    void repeatedTest(RepetitionInfo repetitionInfo) {
-        var p = Persona.builder()
-                .id(repetitionInfo.getCurrentRepetition())
-                .nombre("Daniel" + (repetitionInfo.getCurrentRepetition() % 3 == 0 ? "" : repetitionInfo.getCurrentRepetition()))
-                .apellidos("Jaimes").build();
+                assertAll("Validar propiedades", () -> assertEquals(1, p.getId()), () -> assertEquals("Daniel",
+                        p.getNombre()), () -> assertEquals("Jaimes", p.getApellidos()));
+            }
 
-        assertNotNull(p);
-        assertTrue(p instanceof Persona, "No es instancia de persona");
-        assertAll("Validar propiedades",
-                () -> assertEquals(repetitionInfo.getCurrentRepetition(), p.getId(), "id"),
-                () -> assertEquals("Daniel" + repetitionInfo.getCurrentRepetition(), p.getNombre(), "getNombre"),
-                () -> assertEquals("Jaimes", p.getApellidos(), "getApellidos")
-        );
+            @RepeatedTest(value = 5, name = "{displayName} {currentRepetition} / {totalRepetitions}")
+            void repeatedTest(RepetitionInfo repetitionInfo) {
+                var p = Persona.builder().id(repetitionInfo.getCurrentRepetition()).nombre("Daniel" + (repetitionInfo.getCurrentRepetition() % 3 == 0 ? "" : repetitionInfo.getCurrentRepetition())).apellidos("Jaimes").build();
+
+                assertNotNull(p);
+                assertTrue(p instanceof Persona, "No es instancia de persona");
+                assertAll("Validar propiedades", () -> assertEquals(repetitionInfo.getCurrentRepetition(), p.getId(),
+                        "id"), () -> assertEquals("Daniel" + repetitionInfo.getCurrentRepetition(), p.getNombre(),
+                        "getNombre"), () -> assertEquals("Jaimes", p.getApellidos(), "getApellidos"));
+            }
+            //TODO - Personalized Test
+
+        }
+
+        @Nested
+        public class KO {
+
+        }
     }
 }
