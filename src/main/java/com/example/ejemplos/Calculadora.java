4f2a32c508b6f3e4102c3350a6736b7234ebb601
Daniel Jaimes Carrillo <daniel.jaimes-carrillo@capgemini.com>
2023-03-23 10:07:54 +0100

    Throw ArithmeticException if "b" entry paramter is zero

--- src/main/java/com/example/ejemplos/Calculadora.java
+++ src/main/java/com/example/ejemplos/Calculadora.java
@@ -1,11 +1,21 @@
 package com.example.ejemplos;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Calculadora {
     public double suma(double a, double b) {
-        return a + b;
+        BigDecimal rslt = BigDecimal.valueOf(a + b);
+        return rslt.setScale(15, RoundingMode.HALF_DOWN).doubleValue();
     }
 
     public double divide(double a, double b) {
+        Utils.isZero(b);
+        return a / b;
+    }
+
+    public double divide(int a, int b) {
+        Utils.isZero(b);
         return a / b;
     }
 }
---
+++ src/main/java/com/example/ejemplos/Utils.java
@@ -0,0 +1,7 @@
+package com.example.ejemplos;
+
+public class Utils {
+    static void isZero(double a) throws ArithmeticException {
+        if (a == 0) throw new ArithmeticException("Divide entre 0");
+    }
+}
