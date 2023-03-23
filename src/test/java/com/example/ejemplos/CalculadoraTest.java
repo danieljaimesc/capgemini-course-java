ced90b6d95521546d79c2f3ae6ba957b589f9700
Daniel Jaimes Carrillo <daniel.jaimes-carrillo@capgemini.com>
2023-03-23 10:07:01 +0100

    Add annotations to split types of methods in KO & OK and there metheod tested

--- src/test/java/com/example/ejemplos/CalculadoraTest.java
+++ src/test/java/com/example/ejemplos/CalculadoraTest.java
@@ -1,58 +1,81 @@
 package com.example.ejemplos;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculadoraTest {
+    private Calculadora calc;
 
     @BeforeEach
     void setUp() throws Exception {
-
+        calc = new Calculadora();
     }
 
-    @Test
-    void testSuma() {
-        var calc = new Calculadora();
-        var rslt = calc.suma(2, 2);
-        assertEquals(4, rslt);
+
+    @Nested
+    @DisplayName("Method Sum Test")
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class Suma {
+        @Nested
+        class OK {
+            @Test
+            void test_Suma() {
+                var rslt = calc.suma(2, 2);
+                assertEquals(4, rslt);
+            }
+
+            @Test
+            void test_Suma_Positivo_Negativo() {
+                var rslt = calc.suma(3, -1);
+                assertEquals(2, rslt);
+            }
+
+            @Test
+            void test_Suma_Negativo_Positivo() {
+                var rslt = calc.suma(-3, 3);
+                assertEquals(-0, rslt);
+            }
+
+            @Test
+            void test_Suma_Negativo_Negativo() {
+                var rslt = calc.suma(-4, -3);
+                assertEquals(-7, rslt);
+            }
+
+            @Test
+            void test_Suma_Decimal() {
+                var rslt = calc.suma(0.2, 0.3);
+                assertEquals(0.5, rslt);
+            }
+        }
+
+        @Nested
+        class KO {
+
+        }
     }
 
-    @Test
-    void testSumaPositivoNegativo() {
-        var calc = new Calculadora();
-        var rslt = calc.suma(3, -1);
-        assertEquals(2, rslt);
+    @Nested
+    class Divide {
+        @Nested
+        class OK {
+            @Test
+            void testDividirPorCero() {
+                var rslt = calc.divide(1, 0.0);
+                assertEquals(Double.POSITIVE_INFINITY, rslt);
+            }
+        }
+
+        @Nested
+        class KO {
+            @Test
+            void testDividirPorCero() {
+                assertThrows(ArithmeticException.class, () -> calc.divide(1, 0.0));
+            }
+        }
     }
 
-    @Test
-    void testSumaNegativoPositivo() {
-        var calc = new Calculadora();
-        var rslt = calc.suma(-3, 3);
-        assertEquals(-0, rslt);
-    }
 
-    @Test
-    void testSumaNegativoNegativo() {
-        var calc = new Calculadora();
-        var rslt = calc.suma(-4, -3);
-        assertEquals(-7, rslt);
-    }
-
-    @Test
-    void testSumaDecimal() {
-        var calc = new Calculadora();
-        var rslt = calc.suma(0.2, 0.3);
-        assertEquals(0.5, rslt);
-    }
-
-    @Test
-    void testDividirPorCero() {
-        var calc = new Calculadora();
-
-        var rslt = calc.divide(1, 0.0);
-
-        assertEquals(Double.POSITIVE_INFINITY, rslt);
-    }
 }
