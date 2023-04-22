7f9f31e5e7e2d91ccf41f883bfabcd5dc8c5a796
Daniel Jaimes <daniel.jaimes-carrillo@capgemini.com>
2023-04-23 00:23:51 +0200

    add cors global config

--- SpringSakilaInventory/src/main/java/com/springsakila/InventoryApplication.java
+++ SpringSakilaInventory/src/main/java/com/springsakila/InventoryApplication.java
@@ -3,6 +3,9 @@ package com.springsakila;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class InventoryApplication implements CommandLineRunner {
@@ -14,5 +17,15 @@ public class InventoryApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Application started");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
